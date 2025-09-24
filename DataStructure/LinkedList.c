2b162bd87f983b93fdb7418367462bee7ff2a8fe
Dan <space12590@gmail.com>
2025-09-24 17:09:08 +0900

    Start LinkedList

---
+++ DataStructure/LinkedList.c
@@ -0,0 +1,6 @@
+#include "LinkedList.h"
+
+void Initialize(List* list) 
+{
+    
+}
---
+++ DataStructure/LinkedList.h
@@ -0,0 +1,53 @@
+#ifndef ___LinkedList
+#define ___LinkedList
+
+#include "Member.h"
+
+typedef struct __node
+{
+    Member data;
+    struct __node *next;
+} Node;
+
+typedef struct
+{
+    Node *head;
+    Node *crnt;
+} List;
+
+//링크드리스트 초기화
+void Initialize(List *list);
+
+//compare함수로 x와 같은 노드를 검색
+Node* search(List *list, const Member *x, int compare(const Member *x, const Member *y));
+
+//머리에 노드를 삽입
+void InsertFront(List *list, const Member *x);
+
+//꼬리에 노드를 삽입
+void InsertRear(List *list, const Member *x);
+
+//머리 노드를 삭제
+void RemoveFront(List *list);
+
+//꼬리 노드를 삭제
+void RemoveRear(List *list);
+
+//선택한 노드를 삭제
+void RemoveCurrent(List *list);
+
+//모든 노드를 삭제
+void Clear(List* list);
+
+//선택한 노드의 데이터를 출력
+void PrintCurrent(const List *list);
+
+//선택한 노드의 데이터를 출력 (줄바꿈)
+void PrintLnCurrent(const List *list);
+
+//모든 노드의 데이터를 리스트 순서대로 출력
+void Print(const List *list);
+
+//링크드리스트 종료
+void Terminate(List *list);
+#endif

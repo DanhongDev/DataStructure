53ad4f497351a96d8fefab68895c8ef92564dd23
Dan <space12590@gmail.com>
2026-08-31 12:53:24 +0900

    26.08.28

--- Programmers/Problems/[Lv.2] 3xn 타일링.cpp
+++ Programmers/Problems/[Lv.2] 3xn 타일링.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int solution(int n)
 {
-    int answer = 0;
+    if(n%2==1) return 0;
 
+    vector<long long> dp(n+1 ,0);
+    dp[2] = 3, dp[4] = 11;
+    for(int i=6; i<=dp.size(); i++)
+    {
+        if(i%2==1)
+        {
+            continue;
+        }
+        dp[i] = (((dp[i-2] * 4) % 1000000007) - (dp[i-4] % 1000000007) + 1000000007) % 1000000007;
+    }
     
-    
-    return answer;
+    return dp[n];
 }
---
+++ Programmers/Problems/[Lv.2] 힌트 스테이지.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> cost, vector<vector<int>> hint)
+{
+    int answer = 0;
+
+    
+    
+    return answer;
+}

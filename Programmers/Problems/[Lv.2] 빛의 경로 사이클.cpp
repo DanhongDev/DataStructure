7914ff10580b36ca80058d7551cfc5a51a621089
Dan <space12590@gmail.com>
2026-09-15 16:57:43 +0900

    26.09.15

--- Programmers/Problems/[Lv.2] 빛의 경로 사이클.cpp
+++ Programmers/Problems/[Lv.2] 빛의 경로 사이클.cpp
@@ -1,71 +1,49 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<string> grid)
 {
     vector<int> answer;
 
-    // 0과 size() 인덱스는 통과 용으로 쓰기.
-    vector<string> board(grid.size()+2);
+    int R = grid.size();
+    int C = grid[0].size();
     
-    int dx=1, dy=1;
-    // 방향 전환
-    if(board[dx][dy] == 'L')
+    int dx[] = {-1, 0, 1, 0};
+    int dy[] = {0, 1, 0, -1};
+    
+    vector<vector<vector<int>>> visited(R, vector<vector<int>>(C, vector<int>(4, 0)));
+    
+    for(int i=0; i<R; i++)
     {
-        if(dx==0 && dy==1)
+        for(int j=0; j<C; j++)
         {
-            dx=-1;
-            dy=0;
-        }
-        else if(dx==0 && dy==-1)
-        {
-            dx=1;
-            dy=0;
-        }
-        else if(dx==1 && dy==0)
-        {
-            dx=0;
-            dy=1;
-        }
-        else if(dx==-11 && dy==0)
-        {
-            dx=0;
-            dy=-1;
-        }
-    }
-    else if(board[dx][dy] == 'R')
-    {
-        if(dx==0 && dy==1)
-        {
-            dx=1;
-            dy=0;
-        }
-        else if(dx==0 && dy==-1)
-        {
-            dx=-1;
-            dy=0;
-        }
-        else if(dx==1 && dy==0)
-        {
-            dx=0;
-            dy=-1;
-        }
-        else if(dx==-11 && dy==0)
-        {
-            dx=0;
-            dy=1;
-        }
-    }
+            for(int k=0; k<4; k++)
+            {
+                if(visited[i][j][k]) continue;
 
+                int x=i, y=j, d=k;
+                int len = 0;
+                
+                while(!visited[x][y][d])
+                {
+                    visited[x][y][d] = 1;
+                    len++;
+                    
+                    // 방향 전환
+                    if(grid[x][y] == 'L') d = (d+3) % 4; //왼쪽 90도 회전
+                    else if(grid[x][y] == 'R') d = (d+1) % 4; //오른쪽 90도 회전
+
+                    x = (x + dx[d] + R) % R;
+                    y = (y + dy[d] + C) % C;
+                }
+
+                answer.push_back(len);
+            }
+        }
+    }
     
+    sort(answer.begin(), answer.end());
     return answer;
-}
-
-int main()
-{
-    vector<int> answer = solution({"SL", "LR"});
-    
-    return 0;
 }
